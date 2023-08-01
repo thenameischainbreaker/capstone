e91a8e29f38216558986581da8f6c9e1486502cb
Jasper Javillo <jasperjavillo@gmail.com>
2023-08-01 18:27:40 -0400

    Applied basic CSS and started on typescipt structure of components

--- CapstoneAngular/src/app/app.module.ts
+++ CapstoneAngular/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductContainerComponent } from './product-container/product-container.component';
-import { ProductComponent } from './product/product.component';
-import { CategoryButtonContainerComponent } from './category-button-container/category-button-container.component';
-import { CategoryButtonComponent } from './category-button/category-button.component';
-import { CartComponent } from './cart/cart.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProductContainerComponent } from '../product-container/product-container.component';
+import { ProductComponent } from '../product/product.component';
+import { CategoryButtonContainerComponent } from '../category-button-container/category-button-container.component';
+import { CategoryButtonComponent } from '../category-button/category-button.component';
+import { CartComponent } from '../cart/cart.component';
+import { ProfileComponent } from '../profile/profile.component';
+import { HomeComponent } from '../home/home.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ProfileComponent } from './profile/profile.component';
     CategoryButtonContainerComponent,
     CategoryButtonComponent,
     CartComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
--- CapstoneAngular/src/product/product.component.ts
+++ CapstoneAngular/src/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
+  @Input()pId = 0;
+  @Input()pName="Product Name ";
+  @Input()imageId="Image ID";
+  @Input()pPrice=0.0;
+  @Input()pDescription="Description";
+  
+
+}
+
+export class product{
+  id=0;
+  name="";
+  imageId="";
+  price=0.0;
+  description="";
 
 }
