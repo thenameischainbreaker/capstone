320b1ff35a75a95b2693d43e846ed6689fbfee01
Jasper Javillo <jasperjavillo@gmail.com>
2023-08-03 11:11:59 -0400

    Finished UI Structure

--- CapstoneAngular/src/admin/admin.component.ts
+++ CapstoneAngular/src/admin/admin.component.ts
@@ -17,49 +17,68 @@ export class AdminComponent {
   userList: user[] = [new user()];
   addUser(uf:NgForm){
     //call UserService.addUser to add User to repository
+    console.log("addUser");
+    console.log(uf.value);
   }
   updateUser(uf:NgForm){
     //call UserService.updateUser to add User in repository
+    console.log("udateUser");
+    console.log(uf.value);
   }
   deleteUser(id:number){
     //call UserService.deleteUser to remove User from repository
+    console.log("deleteUser");
+    console.log(id);
   }
   displayUsers(){
     //calls UserService.getUsers to get all User data from repository to display
+    console.log("displayUser");
   }
 
   prodList: product[] = [new product()];
   addProduct(pf:NgForm){
     //calls ProduceService.addProduct to add Product to repository
+    console.log("addProduct");
+    console.log(pf.value);
   }
   updateProduct(pf:NgForm){
     //calls ProductService.updateProduct to update Product in repository
+    console.log("updateProduct");
+    console.log(pf.value);
   }
   deleteProduct(id:number)
   {
     //calls ProductService.deleteProduct to remove Product from repository
+    console.log("deleteProduct");
   }
   displayProduct(){
     //calls ProductService.getProduct to get all Product data from repository to display
+    console.log("displayProduct");
   }
 
   stockList: stock[] = [new stock()]
   displayStock(){
     //calls StockService.getStock to get all Stock counts from repsitory to display
+    console.log("displayStock");
   }
   updateStock(sf:NgForm){
     //call StockService.updateStock to update a Stock count
+    console.log("updateStock");
+    console.log(sf.value);
   }
 
   ordersList: orders[] = [new orders];
   getOrders(of:NgForm){
     //call OrdersService.getOrdersDate to get orders of a specific date
-    console.log(of.value.date);
+    console.log("getOrders");
+    console.log(of.value.startDate);
+    console.log(of.value.endDate);
   }
 
 
   issueDiscount(df:NgForm){
     //call CouponService.postDiscount to post a discount to a specific user
+    console.log("issueDiscount");
     console.log(df.value);
   }
 }
--- CapstoneAngular/src/cart/cart.component.ts
+++ CapstoneAngular/src/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { orders } from 'src/admin/admin.component';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-
+  ordersList: orders[] = [new orders()]
+  constructor(){
+    this.getOrders();
+  }
+  getOrders(){
+    //call a service to insert the orders of a user into ordersList
+    console.log("getOrders()");
+  }
+  sendOrder(){
+    //call a service to finalize the orders in cart
+    console.log("sendOrder()");
+  }
 }
